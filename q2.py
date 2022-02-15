5c5ced6bdd6291cbddb75ebd31a215743f583ba3
Leonardo <leonoman07@gmail.com>
2022-02-15 07:18:09 -0300

    q2 script

---
+++ q2.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import pandas as pd
+import key
+
+query = """query {
+  search(query: "stars:>1", type: REPOSITORY, first: 100) {
+    nodes {
+      ... on Repository {
+        mergedPullRequests: pullRequests(states: MERGED) {
+          totalCount
+        }
+      }
+    }
+  }
+}"""
+
+url = 'https://api.github.com/graphql'
+headers = {"Authorization": "Bearer " + key.AUTH_TOKEN}
+r = requests.post(url, headers=headers,json={'query': query})
+
+# print(r.text) 
+
+json_data = json.loads(r.text)
+
+df_data = json_data['data']['search']['nodes']
+df = pd.DataFrame(df_data)
+df.to_excel("q2.xlsx")
